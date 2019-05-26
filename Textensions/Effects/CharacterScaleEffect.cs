69e0d79b376f7a3b8477b81bc02c05f0485b43c0
Adrian Miasik <AdrianMiasik@gmail.com>
2019-05-26 08:06:25 -0400

    Add effect example

---
+++ Textensions/Effects/CharacterScaleEffect.cs
@@ -0,0 +1,27 @@
+﻿using Textensions.Core;
+using Textensions.Effects.Base;
+using UnityEngine;
+
+namespace Textensions.Effects
+{
+    public class CharacterScaleEffect : MonoBehaviour
+    {
+        public Textension textension;
+        public Effect fx;
+
+        private void Reset()
+        {
+            textension = GetComponent<Textension>();
+        }
+
+        private void Start()
+        {
+            AddEffectToCharacters();
+        }
+
+        private void AddEffectToCharacters()
+        {
+            textension.effectsToApply.Add(fx);
+        }
+    }
+}
