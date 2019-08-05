e4a95267c79d50f1033e1c5e27f37ce457e1927b
Adrian Miasik <adrianmiasik@gmail.com>
2019-08-05 03:28:54 -0400

    Removed unused comments within Scale.cs
    The ability to destroy effects and detect when effects are over will be in another commit later today.

--- Textensions/Reveals/Effects/Scale.cs
+++ Textensions/Reveals/Effects/Scale.cs
@@ -11,44 +11,9 @@ namespace Textensions.Reveals.Effects
 	[CreateAssetMenu(menuName = "Text Reveals/Effects/Scale", fileName = "New Scale Effect")]
 	public class Scale: Effect
 	{
-//        public bool isEffectOver = false;
-
         public override float Calculate(Character c)
 		{
-//            Debug.Log(isEffectOver);
-//            if (isEffectOver)
-//            {
-//                // TODO: Destroy effect
-//                Debug.Log("TODO: Destroy effect");
-//                return 0f;
-//            }
-////            
-			// If our current character is not revealed or not visible, then skip it.
-            if (!c.isRevealed || !c.Info().isVisible) return 0f;
-
-            // Accumulate time to this specific character
-			c.timeSinceReveal += Time.deltaTime;
-            
-            // TODO: Detect when this effect is over (We will then take that data to remove certain effects that are not playing)
-
-//            if (cachedCalculatedValue - uniform.Evaluate(c.timeSinceReveal) == 0)
-//            {
-//                isEffectOver = true;
-//                Debug.Log("No change in previous frame");
-//            }
-
-//            Debug.Log(uniform[uniform.length - 1].time);
-
-//            if (c.timeSinceReveal > uniform[uniform.length - 1].time)
-//            {
-//                isEffectOver = true;
-//                Debug.Log("Effect has completed");
-//                isEffectOver = true;
-//            }
-            
-            cachedCalculatedValue = uniform.Evaluate(c.timeSinceReveal);
-            
-			return cachedCalculatedValue;
+            return uniform.Evaluate(c.timeSinceReveal);
 		}
 	}
 }
