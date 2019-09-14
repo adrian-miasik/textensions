1fa607945c8326c65351293534670dce181c0d8d
Adrian Miasik <adrianmiasik@gmail.com>
2019-09-14 17:52:21 -0400

    Fix whitespaces in ColorRandomWriter.cs

--- Textensions/Reveals/ColorRandomWriter.cs
+++ Textensions/Reveals/ColorRandomWriter.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Textensions.Core;
 using Textensions.Utilities;
@@ -12,17 +12,17 @@ namespace Textensions.Reveals
     /// </summary>
     /// <summary>
     /// Color Reveal: Characters are being rendered by TMP but are hidden by changing the alpha of each character mesh to zero.
-    /// The way we are revealing each character is by altering the color of the vertices on the characters mesh. Each character mesh has 4 vertices and we are changing the color32 of each corner at once.     
+    /// The way we are revealing each character is by altering the color of the vertices on the characters mesh. Each character mesh has 4 vertices and we are changing the color32 of each corner at once.
     /// </summary>
     public class ColorRandomWriter : ColorTypeWriter
     {
         private bool hasBeenInit = false;
-        
+
         protected override void RevealCharacter(int revealNumber)
         {
             // Get random character within the unrevealed characters list
             int characterIndex = Random.Range(0, textension.unrevealedCharacters.Count);
-            
+
             // Color
             ColorSingleCharacter(textension.unrevealedCharacters[characterIndex].Info(), textension.GetCachedColor());
 
@@ -30,11 +30,11 @@ namespace Textensions.Reveals
             // Update the color on the mesh
             textension.text.textInfo.meshInfo[0].mesh.colors32 = textension.text.textInfo.meshInfo[0].colors32;
             textension.text.UpdateGeometry(textension.text.textInfo.meshInfo[0].mesh, 0);
-            
+
 #if DEBUG_TEXT
             Debug.Log("Character: " + textension.unrevealedCharacters[0].Info().character + " has been revealed by ColorRandomWriter.cs [" + GetInstanceID() + "]");
 #endif
-            
+
             // Mark the character as revealed
             MarkAsRevealed(characterIndex);
         }
