f6472602f3ddba2b3709d365cf2217e9554761be
Adrian Miasik <adrianmiasik@gmail.com>
2019-09-14 17:50:43 -0400

    Add comments to all the functions in Character.cs

--- Textensions/Core/Character.cs
+++ Textensions/Core/Character.cs
@@ -58,24 +58,39 @@ namespace Textensions.Core
             isDirty = true;
         }
         
+        /// <summary>
+        /// Adds a vector3 to our cached position.
+        /// </summary>
+        /// <param name="position"></param>
         public void AddPosition(Vector3 position)
         {
             hasPositionUpdated = true;
             _positionCached += position;
         }
 
+        /// <summary>
+        /// Subtracts a vector3 from our cached position.
+        /// </summary>
+        /// <param name="position"></param>
         public void RemovePosition(Vector3 position)
         {
             hasPositionUpdated = true;
             _positionCached -= position;
         }
 
+        /// <summary>
+        /// Directly sets the position of this character.
+        /// </summary>
+        /// <param name="position"></param>
         public void SetPosition(Vector3 position)
         {
             hasPositionUpdated = true;
             this.position = position;
         }
 
+        /// <summary>
+        /// If our character position has been dirtied, we will move the character to the new position and reset the cached position.
+        /// </summary>
         public void ApplyPosition()
         {
             if (hasPositionUpdated)
@@ -86,24 +101,39 @@ namespace Textensions.Core
             }
         }
 
+        /// <summary>
+        /// Multiples a quaternion rotation to our cached rotation.
+        /// </summary>
+        /// <param name="rotation"></param>
         public void AddRotation(Quaternion rotation)
         {
             hasRotationUpdated = true;
             _rotationCached *= rotation;
         }
 
+        /// <summary>
+        /// Multiplies the provided rotation with the inverse of our cached rotation.
+        /// </summary>
+        /// <param name="rotation"></param>
         public void RemoveRotation(Quaternion rotation)
         {
             hasRotationUpdated = true;
             _rotationCached = rotation * Quaternion.Inverse(_rotationCached);
         }
 
+        /// <summary>
+        /// Directly sets the rotation of this character.
+        /// </summary>
+        /// <param name="rotation"></param>
         public void SetRotation(Quaternion rotation)
         {
             hasRotationUpdated = true;
             this.rotation = rotation;
         }
 
+        /// <summary>
+        /// If our character rotation has been dirtied, we will rotate the character to our new rotation and reset the cached rotation.
+        /// </summary>
         public void ApplyRotation()
         {
             if (hasRotationUpdated)
@@ -114,24 +144,39 @@ namespace Textensions.Core
             }
         }
 
+        /// <summary>
+        /// Adds a vector3 to our cached scale.
+        /// </summary>
+        /// <param name="scale"></param>
         public void AddScale(Vector3 scale)
         {
             hasScaleUpdated = true;
             _scaleCached += scale;
         }
 
+        /// <summary>
+        /// Subtracts a vector3 from our cached scale.
+        /// </summary>
+        /// <param name="scale"></param>
         public void RemoveScale(Vector3 scale)
         {
             hasScaleUpdated = true;
             _scaleCached -= scale;
         }
 
+        /// <summary>
+        /// Directly sets the scale of this character.
+        /// </summary>
+        /// <param name="scale"></param>
         public void SetScale(Vector3 scale)
         {
             hasScaleUpdated = true;
             this.scale = scale;
         }
 
+        /// <summary>
+        /// If our character scale has been dirtied, we will scale the character to our new scale and reset the cached scale.
+        /// </summary>
         public void ApplyScale()
         {
             if (hasScaleUpdated)
@@ -142,11 +187,17 @@ namespace Textensions.Core
             }
         }
 
+        /// <summary>
+        /// Marks this character as revealed. (Dirty Flag)
+        /// </summary>
         public void Reveal()
         {
             isRevealed = true;
         }
 
+        /// <summary>
+        /// Marks this character as unrevealed. (Dirty Flag)
+        /// </summary>
         public void Unreveal()
         {
             isRevealed = false;
