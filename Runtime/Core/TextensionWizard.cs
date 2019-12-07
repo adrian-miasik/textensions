02e451c732aaccba40ce8a33ffbba76f37867561
Adrian Miasik <adrianmiasik@gmail.com>
2019-12-07 03:33:01 -0500

    Add support to convert the Textension wizard

--- Runtime/Core/TextensionWizard.cs
+++ Runtime/Core/TextensionWizard.cs
@@ -1,9 +1,10 @@
 using Textensions.Editor;
+using UnityEditor;
 using UnityEngine;
 
 namespace Textensions.Core
 {
-	public class TextensionWizard: Textension, IWizard
+	public class TextensionWizard : Textension, IWizard
 	{
 		public enum WizardStates
 		{
@@ -46,6 +47,65 @@ namespace Textensions.Core
 			}
 		}
 
+		/// <summary>
+		/// Resets the wizard state and forces a redraw on the editor reference.
+		/// </summary>
+		[ContextMenu("Convert to Textension")]
+		public void ConvertToTextension()
+		{
+			if (editor != null)
+			{				
+				int stepCount = MoveComponentToTop(this);
+				UnityEditorInternal.ComponentUtility.CopyComponent(this);
+
+				int stepsToMoveUp = ((GetComponents<Component>().Length - 1) - stepCount) - 1; // Ignore first element, then account for index
+				
+				// Create a new textension and cache it
+				Textension createdTextension = gameObject.AddComponent<Textension>();
+
+				for (int i = 0; i < stepsToMoveUp; i++)
+				{
+					UnityEditorInternal.ComponentUtility.MoveComponentUp(createdTextension);
+				}
+
+				UnityEditorInternal.ComponentUtility.PasteComponentValues(createdTextension);
+				
+				// Destroy self
+				DestroyImmediate(this); // This spits out an instance id error
+			}
+		}
+
+		private int MoveComponentToTop(Textension textension)
+		{
+			Component[] allComponents = GetComponents<Component>();
+			
+			// You can't move anything above the transform component.
+			// We need to get the 1st component under that.
+			int topMostInstanceID = allComponents[1].GetInstanceID();
+			
+			int movingComponentInstanceID = textension.GetInstanceID();
+
+			int timesMovedUpwards = 0;
+
+			for (int i = 0; i < allComponents.Length; i++)
+			{
+				if (topMostInstanceID != movingComponentInstanceID)
+				{
+					timesMovedUpwards++;
+					UnityEditorInternal.ComponentUtility.MoveComponentUp(textension);
+
+					// Fetch a refreshed order of the components.
+					topMostInstanceID = GetComponents<Component>()[1].GetInstanceID();
+				}
+				else
+				{
+					return timesMovedUpwards;
+				}
+			}
+
+			return timesMovedUpwards;
+		}
+		
 		public void CompleteWizard()
 		{
 			hasWizardCompleted = true;
