acce46bea30277e7b7de405c311753c935773652
Adrian Miasik <adrianmiasik@gmail.com>
2019-12-09 01:33:29 -0500

    Fix the copy and paste functionality in the ComponentClipboard

--- Runtime/Utilities/ComponentClipboard.cs
+++ Runtime/Utilities/ComponentClipboard.cs
@@ -3,66 +3,74 @@ using UnityEngine;
 
 namespace Textensions.Utilities
 {
-	public static class ComponentClipboard
-	{
-		private static SerializedObject copiedObject;
+    public static class ComponentClipboard
+    {
+        private static SerializedObject copiedObject;
 		
-		/// <summary>
-		/// Create a serialized copy of our target component.
-		/// </summary>
-		/// <param name="_component"></param>
-		public static void CopyComponent(Component _component)
-		{
-			copiedObject = new SerializedObject(_component);
-		}
+        /// <summary>
+        /// Create a serialized copy of our target component.
+        /// </summary>
+        /// <param name="_component"></param>
+        public static void CopyComponent(Component _component)
+        {
+            copiedObject = new SerializedObject(_component);
+        }
+        
+        /// <summary>
+        /// Paste any matching property/properties from our copied object component to our target component.
+        /// </summary>
+        /// <param name="_component"></param>
+        public static void PasteComponent(Component _component)
+        {
+            SerializedObject target = new SerializedObject(_component);
+            SerializedProperty iterator = copiedObject.GetIterator();
 
-		/// <summary>
-		/// Paste any matching property/properties from our copied object component to our target component.
-		/// </summary>
-		/// <param name="_component"></param>
-		public static void PasteComponent(Component _component)
-		{
-			SerializedObject target = new SerializedObject(_component);
-			SerializedProperty iterator = copiedObject.GetIterator();
+            // If we are iterating through properties, we can ignore the first property reference;
+            // Which is the script reference.
+            int numberOfVisibleProperties = iterator.CountRemaining() - 1;
 
-			int numberOfVisibleProperties = iterator.CountRemaining();
-			iterator.Reset(); // Needs to be reset when calling CountRemaining.
+            // The iterator needs to be reset when calling CountRemaining.
+            iterator.Reset();
+
+            // Skip and check our first reference
+            if (iterator.NextVisible(true).GetType() == copiedObject.GetType())
+            {
+                Debug.Log("These are the same objects");
+                // TODO: Native Unity copy and paste
+                return;
+            }
             
-            // TODO: Check if the component is exactly the same instead of checking all properties
-            
-			for (int i = 0; i < numberOfVisibleProperties; i++)
-			{
-				// If we have a visible property...
+            for (int i = 0; i < numberOfVisibleProperties; i++)
+            {
+                // If we have a visible property...
                 if (iterator.NextVisible(true))
                 {
-					SerializedProperty property = target.FindProperty(iterator.name);
+                    SerializedProperty property = target.FindProperty(iterator.name);
 
-					// If our property can't be found, move on to the next one.
-					if (property == null) 
-						continue;
+                    // If our property can't be found, move on to the next property
+                    if (property == null) 
+                        continue;
+
+                    // If our types don't match, move on to the next property
+                    if (!DoPropertyTypesMatch(property, iterator)) 
+                        continue;
                     
-					if (DoPropertyTypesMatch(property, iterator))
-					{
-                        Debug.Log("Attempting to copy variable: " + property.displayName);
-                        
-                        // TODO: Fix copy
-						target.CopyFromSerializedProperty(iterator);
-					}
-				}
-				// No more visible properties found
-				else
-				{
-					iterator.Reset();
-					break;
-				}
-			}
+                    target.CopyFromSerializedProperty(iterator);
+                }
+                // No more visible properties found
+                else
+                {
+                    iterator.Reset();
+                    break;
+                }
+            }
 
-			target.ApplyModifiedProperties();
-		}
+            target.ApplyModifiedProperties();
+        }
 
-		private static bool DoPropertyTypesMatch(SerializedProperty _propertyA, SerializedProperty _propertyB)
-		{
-			return _propertyA.propertyType == _propertyB.propertyType;
-		}
-	}
+        private static bool DoPropertyTypesMatch(SerializedProperty _propertyA, SerializedProperty _propertyB)
+        {
+            return _propertyA.propertyType == _propertyB.propertyType;
+        }
+    }
 }
